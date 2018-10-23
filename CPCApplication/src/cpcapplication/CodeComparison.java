d8841a114eee655526e48b1a28a90ac13d0fde4f
jsdaniel007 <chrismcclure360@gmail.com>
2018-10-23 17:08:40 -0400

    Initial Setup Commit

---
+++ CPCApplication/src/cpcapplication/CodeComparison.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cpcapplication;
+
+/**
+ *
+ * @author jsdan
+ */
+public class CodeComparison {
+    
+    public static void Compare() {
+    }
+    
+    public static void Stage1(String line1, String line2) {
+        
+    }
+    
+    public static void Stage2(String line1, String line2) {
+        
+    }
+    
+}
