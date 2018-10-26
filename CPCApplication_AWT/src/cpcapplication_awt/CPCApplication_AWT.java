6ebb1bc372ea50fa1e4c125dfc0c51e0d7c2cc37
jsdaniel007 <chrismcclure360@gmail.com>
2018-10-25 23:35:47 -0400

    WIP GUI commit, GridPane formatting needed

--- CPCApplication_AWT/src/cpcapplication_awt/CPCApplication_AWT.java
+++ CPCApplication_AWT/src/cpcapplication_awt/CPCApplication_AWT.java
@@ -9,9 +9,14 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.geometry.Pos;
+import javax.swing.*;
+import java.awt.*;
+import javafx.geometry.Insets;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
@@ -23,41 +28,78 @@ import javafx.stage.Stage;
  */
 public class CPCApplication_AWT extends Application {
     
+    private Label CompareTitleLabel;
+    
     //start of the Program from a GUI perspective
     @Override
     public void start(Stage primaryStage) {
-        //GUI code goes here
-        //Hbox def
         
-        //Label def
-        Label ComparisonLabel = new Label("Comparison");
+        //Create a Gridpane to add panels to, for setting up the page.
+        GridPane gridPane = new GridPane();
+        //ColumnConstraints topLeftColumn = new ColumnConstraints();
+        gridPane.getColumnConstraints().add(new ColumnConstraints(100));
+        gridPane.getColumnConstraints().add(new ColumnConstraints(200));
         
-        //button definition + the Event Handler
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        //Button Event Handler 
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                //What happens when the button is pushed
-                System.out.println("Hello World!");
-            }
-        });
+        //Create the title label
+        Label ComparisonTitle = new Label("Comparisons");
+        //Create the "Go to Database Page" Button
+        Button DataPageButton = new Button("Go To Database Page");
+        //Create a Hbox and for the ComparisonTitle
+        HBox titleHBox = new HBox(30, ComparisonTitle, DataPageButton);
+        //Create a VBox fo the TitleBox
+        VBox titleVBox = new VBox(20, titleHBox, DataPageButton, ComparisonTitle, gridPane);
         
-        //StackPane is???
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        //titleVBox.setPadding(new Insets(3));
         
-        Scene scene = new Scene(root, 600, 500);
+        titleVBox.setAlignment(Pos.TOP_CENTER);
         
-        //primaryStage is what is shown, when the curtain is pulled back
-        primaryStage.setTitle("Hello World!");
+        //create 4 panels with its label and such for the layout in the GridLayout
+        Label topLeftPanLabel = new Label("Choose File 1 Below to Upload");
+        HBox topLeftPanHBox = new HBox(30, topLeftPanLabel);
+        VBox topLeftPanVBox = new VBox(30, topLeftPanHBox);
+        
+        Label topRightPanLabel = new Label("Choose File 2 Below to Upload");
+        HBox topRightPanHBox = new HBox(30, topRightPanLabel);
+        VBox topRightPanVBox = new VBox(30, topRightPanHBox);
+        
+        
+        Label botLeftPanLabel = new Label("Plagiarism Checker");
+        HBox botLeftPanHBox = new HBox(30, botLeftPanLabel);
+        VBox botLeftPanVBox = new VBox(30, botLeftPanHBox);
+        
+        Label botRightPanLabel = new Label("Paste Code Below for File 1 Comparison");
+        HBox botRightPanHBox = new HBox(30, botRightPanLabel);
+        VBox botRightPanVBox = new VBox(30, botRightPanHBox);
+        
+        //add to the panels
+        //gridpane additions with (component, col, row)
+        gridPane.add(topLeftPanVBox, 0, 0);
+        gridPane.add(topRightPanVBox, 1, 0);
+        gridPane.add(botLeftPanVBox, 2,1);
+        gridPane.add(botRightPanVBox, 2, 2);
+        //topLeftPan.add(topLeftPanLabel);
+        
+        //Top Right Panel additions
+        //topRightPan.add(topRightPanLabel);
+        
+        //Bottom Left Panel additions
+        //botLeftPan.add(botLeftPanLabel);
+        
+        //Bottom Right Panel additions
+        //botRightPan.add(botRightPanLabel);
+        
+        Scene scene = new Scene(titleVBox, 600, 600);
+        
+        
+        //problem: resize default size
+        primaryStage.setTitle("CPC Application");
+        //primaryStage.setWidth(400);
         primaryStage.setScene(scene);
-        //curtain being pulled back
         primaryStage.show();
     }
-
+    
+    
+    
     /**
      * @param args the command line arguments
      * 
