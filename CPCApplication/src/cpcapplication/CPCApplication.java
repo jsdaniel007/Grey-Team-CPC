365bed7688f8ba64431282520eb22a57e6cb3da5
jsdaniel007 <chrismcclure360@gmail.com>
2018-11-02 23:29:56 -0400

    Started 2nd Screen Setup

--- CPCApplication/src/cpcapplication/CPCApplication.java
+++ CPCApplication/src/cpcapplication/CPCApplication.java
@@ -10,16 +10,13 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.geometry.Pos;
-import javax.swing.*;
-import java.awt.*;
 import java.io.File;
-import java.util.HashSet;
-import java.util.Set;
 import javafx.collections.ObservableList;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.geometry.Insets;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.CheckBox;
 import javafx.scene.layout.HBox;
@@ -43,42 +40,52 @@ public class CPCApplication extends Application {
     //start of the Program from a GUI perspective
     @Override
     public void start(Stage primaryStage) {
-        //Allow access to your other classes
+    //Allow access to your other classes
         CodeComparison CC = new CodeComparison();
         CompareToLib CTL = new CompareToLib();
         Database DB = new Database();
         
-        //Create a Gridpane to add panels to, for setting up the page.
+    //Create a Gridpane to add panels to, for setting up the page.
         GridPane gridPane = new GridPane();
+        GridPane gridPane2 = new GridPane();
         //gridPane padding and gap settings
         gridPane.setVgap(20);
+        gridPane2.setVgap(20);
         gridPane.setHgap(40);
+        gridPane2.setHgap(40);
         gridPane.setPadding(new Insets(10, 10, 10, 10));
+        gridPane2.setPadding(new Insets(10, 10, 10, 10));
         //gridPane.setGridLinesVisible(true);
+        gridPane2.setGridLinesVisible(true);
         
-        //TITLE- Create the title label
-        Label ComparisonTitle = new Label("Comparisons");
-        //Create the "Go to Database Page" Button-- HANDLE NEEDED
-        Button DataPageButton = new Button("Go To Database Page");
-        //Create a Hbox and for the ComparisonTitle
-        HBox titleHBox = new HBox(20, ComparisonTitle, DataPageButton);
-        //Create a VBox fo the TitleBox
-        VBox titleVBox = new VBox(20, titleHBox, DataPageButton, ComparisonTitle, gridPane);
-        
-        titleVBox.setAlignment(Pos.TOP_CENTER);
-        
-        //FORMATTING- Use for padding out and dividing up the page
+    //FORMATTING- Use for padding out and dividing up the page
         Label xDivider = new Label("==========================");
         
-        //REUSABLE- Create reusable elements for the GUI throughout the Program
+    //TITLE- Create the title labels for the screens
+        Label ComparisonTitle = new Label("Comparisons");
+        Label DatabaseTitle = new Label("Database");
+    //Create the Navigation Buttons-- HANDLE NEEDED
+        Button DataPageButton = new Button("Go To Database Page");
+        Button ComparisonPageButton = new Button("Go To Comparisons Page");
+    //Create a Hbox and for the ComparisonTitle
+        HBox titleHBox = new HBox(20, ComparisonTitle);
+    //Create a VBox fo the TitleBox
+        VBox CompTitleVBox = new VBox(20, ComparisonTitle, DataPageButton, gridPane);
+        VBox DataTitleVBox = new VBox(20, DatabaseTitle, ComparisonPageButton, gridPane2);
+        
+        CompTitleVBox.setAlignment(Pos.TOP_CENTER);
+        DataTitleVBox.setAlignment(Pos.TOP_CENTER);
+        
+        
+    //REUSABLE- Create reusable elements for the GUI throughout the Program
         Button browseButton1 = new Button("Browse...");
         Button browseButton2 = new Button("Browse...");
         CheckBox saveBox = new CheckBox("Save to the Database");
-            saveBox.setIndeterminate(false);
+            saveBox.setVisible(true);
         TextField pastedCodeField = new TextField();
         
-        //FileChooser Setup and Options
-        //File 1 Chooser
+    //FileChooser Setup and Options
+    //File 1 Chooser
         FileChooser file1Chooser = new FileChooser();
             file1Chooser.setTitle("Select File 1");
             file1Chooser.getExtensionFilters().addAll(
@@ -94,14 +101,15 @@ public class CPCApplication extends Application {
                 new ExtensionFilter("All Files", "*.*"));
         
             
-        //create 4 panels with its label and HBoxes & VBoxes for the layout in the GridLayout
-        //Top Left Panel of the Program
+    //create 4 panels with its label and HBoxes & VBoxes for the layout in the GridLayout
+    //Top Left Panel of the Program
         Label topLeftPanLabel = new Label("Choose File 1 Below to Upload");
-        HBox topLeftPanHBox = new HBox(30, topLeftPanLabel);
+        HBox topLeftPanHBox = new HBox(topLeftPanLabel);
+        HBox BrowseButtonHBox = new HBox(browseButton1);
+        HBox SaveCheckHBox = new HBox(saveBox);
         VBox topLeftPanVBox = 
-            new VBox(30);
-        ObservableList topLeftList = topLeftPanVBox.getChildren();
-        topLeftList.addAll(xDivider, topLeftPanHBox, browseButton1, saveBox);
+            new VBox(30, topLeftPanHBox, BrowseButtonHBox, SaveCheckHBox);
+        
         //Top Left Panel Handlers
         browseButton1.setOnAction(new EventHandler<ActionEvent>( ) {
             @Override public void handle(ActionEvent e) {
@@ -115,14 +123,14 @@ public class CPCApplication extends Application {
         });
         
         
-        //Top Right Panel of the Program
+    //Top Right Panel of the Program
         Label topRightPanLabel = new Label("Choose File 2 Below to Upload");
         HBox topRightPanHBox = new HBox(30, topRightPanLabel);
-        //File Chooser should go here
         Button compareButton1 = new Button("Compare File 1 to File 2");
         VBox topRightPanVBox = 
             new VBox(30, xDivider, topRightPanHBox, browseButton2, saveBox, compareButton1);
-        //Top Right Panel Handlers
+        
+    //Top Right Panel Handlers
         browseButton2.setOnAction(new EventHandler<ActionEvent>( ) {
             @Override public void handle(ActionEvent e) {
                 File file2Selection = file1Chooser.showOpenDialog(primaryStage);
@@ -143,14 +151,22 @@ public class CPCApplication extends Application {
            } 
         });
         
-        //Bottom Left Panel of the Program
+        
+    //Bottom Left Panel of the Program
+    //Problem: LogoView will not show
         Label botLeftPanLabel = new Label("Plagiarism Checker");
-        //Image Logo = new Image("/Logo.PNG");
+        Image Logo = new Image("file:Logo.PNG");
+            ImageView LogoView = new ImageView();
+            LogoView.setImage(Logo);
+            LogoView.setFitWidth(100);
+            LogoView.setPreserveRatio(true);
+            LogoView.setSmooth(true);
+            LogoView.setCache(true);
         HBox botLeftPanHBox = new HBox(30, botLeftPanLabel);
-        VBox botLeftPanVBox = new VBox(30, xDivider, botLeftPanHBox);
+        VBox botLeftPanVBox = new VBox(30, xDivider, botLeftPanHBox, LogoView);
         
         
-        //Bottom Right Panel of the Program
+    //Bottom Right Panel of the Program
         Label botRightPanLabel = new Label("Paste Code Below for File 1 Comparison");
         Button compareButton2 = new Button("Compare File 1 to Pasted Code");
         HBox botRightPanHBox = new HBox(30, botRightPanLabel);
@@ -158,52 +174,66 @@ public class CPCApplication extends Application {
             new VBox(30, xDivider, botRightPanHBox, pastedCodeField, saveBox, compareButton2);
         
         
-        //Insert Handlers Here
+    //Insert Handlers Here
         
         
-        
-        //Constraints for the left and right columns
+    //Constraints for the left and right columns
         ColumnConstraints leftColumn = new ColumnConstraints();
         leftColumn.setPercentWidth(100);
         ColumnConstraints rightColumn = new ColumnConstraints();
         rightColumn.setPercentWidth(100);
         
         //Constraints for the First row (0,0 & 1,0) and Second Row (0,1 & 1,1)
-        RowConstraints firstRow = new RowConstraints();
-        firstRow.setPercentHeight(150);
-        RowConstraints secondRow = new RowConstraints();
-        secondRow.setPercentHeight(150);
+        //RowConstraints firstRow = new RowConstraints();
+        //firstRow.setPercentHeight(150);
+        //RowConstraints secondRow = new RowConstraints();
+        //secondRow.setPercentHeight(150);
         
-        //each gridPane column specified with the parameters are applied with the column constraints
+    //each gridPane column specified with the parameters are applied with the column constraints
         gridPane.getColumnConstraints().addAll(leftColumn, rightColumn); 
-        gridPane.getRowConstraints().addAll(firstRow, secondRow);
+        gridPane2.getColumnConstraints().addAll(leftColumn, rightColumn);
+        //gridPane.getRowConstraints().addAll(firstRow, secondRow);
         
         gridPane.add(topLeftPanVBox, 0, 0);
         gridPane.add(topRightPanVBox, 1, 0);
         gridPane.add(botLeftPanVBox, 0, 1);
         gridPane.add(botRightPanVBox, 1, 1);
         
-        //topLeftPan.add(topLeftPanLabel);
+        gridPane2.add();
+        gridPane2.add();
         
-        //Top Right Panel additions
-        //topRightPan.add(topRightPanLabel);
+        //SCENES: Scene Handling
+        Scene Compscene = new Scene(CompTitleVBox, 600, 600);
+        Scene DataScene = new Scene(DataTitleVBox, 600, 600);
         
-        //Bottom Left Panel additions
-        //botLeftPan.add(botLeftPanLabel);
+    //DataPageButton Handler 
+    DataPageButton.setOnAction(new EventHandler<ActionEvent>( ) {
+            @Override public void handle(ActionEvent e) {
+
+                primaryStage.setScene(DataScene);
+                primaryStage.show();
+            }
+        });
+    //ComparisonPageButton Handler
+    ComparisonPageButton.setOnAction(new EventHandler<ActionEvent>( ) {
+            @Override public void handle(ActionEvent e) {   
+                primaryStage.setScene(Compscene);
+                primaryStage.show();
+            }
+        }); 
         
-        //Bottom Right Panel additions
-        //botRightPan.add(botRightPanLabel);
-        
-        Scene scene = new Scene(titleVBox, 600, 600);
-        
-        
-        //problem: resize default size
         primaryStage.setTitle("CPC Application");
-        //primaryStage.setWidth(400);
-        primaryStage.setScene(scene);
+        primaryStage.setScene(Compscene);
         primaryStage.show();
     }
     
+    //Class for the second screen of the program
+    public class DatabaseScreen extends Application {
+        @Override
+        public void start(Stage secondaryStage) {
+        
+        }
+    }
     
     
     /**
